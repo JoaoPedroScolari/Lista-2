f59786329b47f454fd5a5d4d8edfa52a25739237
JoaoPedroScolari <jpscolari59@gmail.com>
2024-03-13 21:46:11 -0300

    adicionando

---
+++ ex 16.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	char conceito;
+	printf("Digite o conceito:\n");
+	scanf("%c", & conceito);
+	
+	switch(conceito){
+		case 'A':
+			printf("Aprendizagem Plena");
+			break;
+		case 'B':
+	    	printf("Parcialmente Plena");
+		    break;
+		case 'C':
+			printf("Suficiente");
+			break;
+		case 'D':
+			printf("Insuficiente");
+			break;
+		default:
+			printf("conceito invalido");
+			
+	}	
+	
+}
---
+++ ex 9.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main (){
+	
+	int idade;
+	
+	printf("DETERMINADO DE FAIXA ETARIA");
+	printf("Digite uma idade\n");
+	scanf("%d", &idade);
+
+	if (idade>60){
+		
+		printf("%d corresponde a idoso", idade);
+		
+	}else{
+		
+		if(idade<=60, idade>=19){
+			
+			printf("%d corresponde a adulto", idade);
+			
+		}else{
+			
+			if(idade<=18, idade>=13){
+				
+				printf("%d corresponde a adolescente", idade);
+				
+			}else{
+				
+				if(idade<=12, idade>=1){
+					
+					printf("%d corresponde a crianca", idade);
+					
+					}else{
+					
+					printf("Idade invalida");
+						
+					}
+					
+			
+					
+				}
+				
+			}
+			
+		}
+	return 0;		
+	}
